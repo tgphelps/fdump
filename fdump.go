4abc2317b8b30874fc2454f170ddb984ea15d7b6
Terry Phelps <tgphelps50@gmail.com>
2019-12-15 20:57:07 -0500

    Start interface to hdump

--- fdump.go
+++ fdump.go
@@ -11,14 +11,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"hdump"
 	"log"
 	"os"
 )
 
 const usageHdr = "usage: fdump -c <count> -o <offset> -h <file>"
+const buffSize = 16 * 8 //must be a multiple of 16
+
+var buffArray [buffSize]byte
+var buff = buffArray[:]
 
 func main() {
-	log.SetFlags(0)
+	log.SetFlags(0) // No date/time in messages
 	var pCount = flag.Int("c", 0, "bytes to dump")
 	var pOffset = flag.Int("o", 0, "file offset of start")
 	var pHexOnly = flag.Bool("h", false, "hex-only dump")
@@ -47,7 +52,15 @@ func dump(path string, count int, offset int, hexOnly bool) {
 	}
 	defer file.Close()
 	fmt.Println("file opened")
-	fmt.Printf("type(file) = %T\n", file)	// type = *os.File
+	fmt.Printf("type(file) = %T\n", file) // type = *os.File
+	dest := hdump.NewHdumper(os.Stdout)
+	// fmt.Printf("type(dest) = %T\n", d)
+	dumpBytes(file, dest)
+}
+
+func dumpBytes (file *os.File, dest hdump.Hdumper) {
+	fmt.Println("dumping data from", file, "to", dest)
+	hdump.DumpBytes(&dest, buff)
 }
 
 func checkFile(path string) {
