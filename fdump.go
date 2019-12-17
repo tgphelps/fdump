f5e01314a98a642c4b19a5379ea97c84fa13dfa8
Terry Phelps <tgphelps50@gmail.com>
2019-12-17 15:28:37 -0500

    All program options work no, maybe.

--- fdump.go
+++ fdump.go
@@ -2,7 +2,7 @@
 Usage: fdump -c <count> -o <offset> -h path
 	-c <count>		Number of bytes to dump
 	-o <offset>		Byte offset to start at
-	-h				Hex dump only. No ASCII.package fdump
+	-h				Hex dump only.
 	path			File to dump
 */
 
@@ -19,13 +19,14 @@ import (
 
 const usageHdr = "usage: fdump -c <count> -o <offset> -h <file>"
 const buffSize = 16 * 4 //must be a multiple of 16
+const maxCount = 1024 * 1024 * 1024
 
 // var buffArray [buffSize]byte
 // var buff = buffArray[:]
 
 func main() {
 	log.SetFlags(0) // No date/time in messages
-	var pCount = flag.Int("c", 0, "bytes to dump")
+	var pCount = flag.Int("c", maxCount, "max bytes to dump")
 	var pOffset = flag.Int("o", 0, "file offset of start")
 	var pHexOnly = flag.Bool("h", false, "hex-only dump")
 	flag.Parse()
@@ -45,7 +46,7 @@ func usage() {
 }
 
 func dump(path string, count int, offset int, hexOnly bool) {
-	fmt.Printf("dumping file %s offset %d count %d\n", path, count, offset)
+	// fmt.Printf("dumping file %s offset %d count %d\n", path, offset, count)
 	checkFile(path)
 	file, err := os.Open(path)
 	if err != nil {
@@ -54,13 +55,20 @@ func dump(path string, count int, offset int, hexOnly bool) {
 	defer file.Close()
 	// fmt.Println("file opened")
 	dest := hdump.NewHdumper(os.Stdout)
-	dumpBytes(file, dest)
+	if hexOnly {
+		dest.SetHexOnly()
+	}
+	dumpBytes(file, count, offset, dest)
 }
 
-func dumpBytes(file *os.File, dest *hdump.Hdumper) {
+func dumpBytes(file *os.File, count int, offset int, dest *hdump.Hdumper) {
 	// fmt.Println("dumping data from", file, "to", dest)
 	buff := make([]byte, buffSize)
-	for {
+	if offset > 0 {
+		dest.SetOffset(offset)
+		file.Seek(int64(offset), io.SeekStart)
+	}
+	for count > 0{
 		num, err := file.Read(buff)
 		if err != nil {
 			if err == io.EOF {
@@ -72,10 +80,14 @@ func dumpBytes(file *os.File, dest *hdump.Hdumper) {
 				log.Fatal("error reading file:", err)
 			}
 		}
+		if num > count {
+			num = count
+		}
 		err = dest.DumpBytes(num, buff)
 		if err != nil {
 			log.Fatal("error writing dump:", err)
 		}
+		count -= num
 	}
 }
 
